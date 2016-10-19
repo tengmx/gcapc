e51ad59d002fd815bef19ecb862f90956fecf843
Mingxiang Teng <tengmx@gmail.com>
2016-10-19 11:02:19 -0400

    first release

---
+++ R/bdwidth.r
@@ -0,0 +1,71 @@
+#' @title ChIP-seq Binding Width Estimation
+#'
+#' @description
+#' ChIP-seq experiments usually use crosslinking strategy to capture
+#' sequencing fragments. The fragment size is affected by at least but
+#' not limited to two factors, protein real binding and crosslinking
+#' operation. This function estimate size of binding part in crosslinked
+#' DNA-protein complexes, and denoted that as ChIP-seq binding width.
+#'
+#' @param cov A list object returned by function \code{rc5end}.
+#'
+#' @param range A non-nagative integer vector with length 2. This vector
+#' set the range within which binding width are estimated.
+#' Default c(50,600) represents most ChIP-seq experiments.
+#'
+#' @param step A non-negative integer to set the resolution of binding
+#' width estimation within \code{range}. This value will be tuned if
+#' \code{auto} is TRUE. Default 50 is based on default value of \code{range}.
+#'
+#' @param auto A logical vector which, when TRUE, allows to automatically
+#' tune to higher resolution of binding width estimation. The highest
+#' resolution of 2bp is allowed. Default: TRUE.
+#'
+#' @return A numeric indicating estimated binding width.
+#'
+#' @import S4Vectors
+#' @import IRanges
+#'
+#' @export
+#' @examples
+#' bam <- system.file("extdata/chipseq.bam",package="gcapc")
+#' cov <- rc5end(bam)
+#' bdw <- bdwidth(cov)
+
+bdwidth <- function(cov,range=c(50L,600L),step=50L,auto=TRUE){
+    cat("Starting to estimate bdwidth.\n")
+    if(!is.list(cov) || length(cov)!=2)
+        stop("bdwidth: cov is not a list of 2 elements\n")
+    if(sum(names(cov) %in% c("fwd","rev"))!=2)
+        stop("bdwidth: names of cov is not correct\n")
+    if(length(cov$fwd)!=length(cov$rev))
+        stop("bdwidth: chroms differ between fwd and rev strands\n")
+    readsum <- sapply(cov$fwd,sum) + sapply(cov$rev,sum)
+    chromlen <- sapply(cov$fwd,length)
+    cycle <- 1
+    repeat{
+        cat("...... cycle",cycle,"for bind width estimation\n")
+        shifts <- seq(range[1],range[2],step)
+        regionpos <- regionneg <- IRangesList()
+        for(i in seq_along(cov$fwd)){
+            regionpos[[i]] <- IRanges(start=rep(1,length(shifts)),
+                              end=chromlen[i]-shifts)
+            regionneg[[i]] <- IRanges(start=shifts+1,
+                              end=rep(chromlen[i],length(shifts)))
+        }
+        viewpos <- Views(cov$fwd,regionpos)
+        viewneg <- Views(cov$rev,regionneg)
+        cors <- sapply(seq_along(viewpos),function(i)
+                       cor(viewpos[[i]],viewneg[[i]]))
+        corsall <- colSums(t(cors) * readsum / sum(readsum))
+        if(!auto | step==2){
+            cat("...... estimated bind width as",
+                shifts[which.max(corsall)],"\n")
+            break
+        }
+        range <- shifts[which.max(corsall)]+c(-step,step)
+        step <- max(round(step/5),2)
+        cycle <- cycle + 1
+    }
+    shifts[which.max(corsall)]
+}
---
+++ R/peakcat.r
@@ -0,0 +1,60 @@
+#' @title CATplot of Peaks
+#'
+#' @description
+#' Plot the consistancy between two peak lists.
+#'
+#' @param x A GRanges of identified peaks from one method or one replicate.
+#' At least one meta column should be included to allow for significance
+#' ranking of peaks.
+#'
+#' @param y A GRanges of identified peaks from compared method or
+#' anoter replicate. At least one meta column should be included to
+#' allow for significance ranking of peaks.
+#'
+#' @param ranks A non-negative integer vector specifying the ranks to
+#' be used for CAT plot.
+#'
+#' @param esx A non-negative integer specifying which meta column of
+#' \code{x} to be used to rank peak significance. Larger values in this
+#' column should indicate higher significance.
+#'
+#' @param esy A non-negative integer specifying which meta column of
+#' \code{y} to be used to rank peak significance. Larger values in this
+#' column should indicate higher significance.
+#'
+#' @param add A logical vector which, when TRUE, adds the current plotting
+#' line to existing plots. FALSE will generate a new plot.
+#'
+#' @param ... Other parameters passed to \code{plot} or \code{lines}.
+#'
+#' @return A CAT plot.
+#'
+#' @import S4Vectors
+#' @import GenomicRanges
+#' @importFrom graphics plot
+#' @importFrom graphics lines
+#'
+#' @export
+#' @examples
+#' bam <- system.file("extdata/chipseq.bam",package="gcapc")
+#' cov <- rc5end(bam)
+#' bdw <- bdwidth(cov)
+#' gcb1 <- gcbias(cov,bdw,samp=0.15,plot=FALSE)
+#' peaks1 <- gcapc(cov,gcb1,bdw)
+#' gcb2 <- gcbias(cov,bdw,samp=0.1,plot=FALSE)
+#' peaks2 <- gcapc(cov,gcb2,bdw)
+#' peakcat(peaks1,peaks2,ranks=seq(100,200,5),ylim=c(0,1))
+
+peakcat <- function(x,y,ranks=seq(200,20000,50),esx=1,esy=1,add=FALSE,...){
+    fo <- findOverlaps(x,y)
+    xfo <- queryHits(fo)
+    yfo <- subjectHits(fo)
+    xo <- order(mcols(x)[,esx],decreasing=TRUE)
+    yo <- order(mcols(y)[,esy],decreasing=TRUE)
+    xycat <- sapply(ranks,function(t){
+        idx <- seq_len(t)
+        sum(xfo %in% xo[idx] & yfo %in% yo[idx])/t
+    })
+    if(add) lines(ranks,xycat,...)
+    else plot(ranks,xycat,type='l',...)
+}
