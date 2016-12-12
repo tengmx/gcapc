abe8c127534a386dff68fb09cf40cb395b34fc2f
Martin Morgan <mtmorgan@fhcrc.org>
2016-12-12 22:17:22 +0000

    Adding chimeraviz, RnaSeqGeneEdgeRQL, gcapc, BUMHMM, sparseDOSSA, clusterSeq, ramwas
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gcapc@125047 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/read5endCoverage.r
@@ -0,0 +1,72 @@
+#' @title Reads Coverage Using 5-end Base
+#'
+#' @description
+#' Reads coverage in single base pair resolution using only 5-prime end
+#' of BAM file records. Coverages are reported for forward and reverse
+#' strands separately. Options for customized filtering of BAM records
+#' are provided.
+#'
+#' @param bam The path to a BAM file, which is sorted and indexed.
+#'
+#' @param chroms NULL or a vector of chromosome names that compatible with
+#' the provided BAM file. Reads coverage will be generated for these
+#' chromosomes. Default (NULL) will use all chromosomes in BAM file.
+#'
+#' @param mapq A non-negative integer specifying the minimum mapping
+#' quality to include. BAM records with mapping qualities less
+#' than \code{mapq} are discarded.
+#'
+#' @param duplicate A logical vector which, when FALSE (Default), returns
+#' maximum coverage of 1 for every base pair. Reads that start at the same
+#' position but on different strands are not treated as duplicates.
+#'
+#' @param flag A returned object by \code{Rsamtools::scanBamFlag}.
+#' Additional options for BAM records filtering.
+#'
+#' @return A list of two objects by \code{GenomicRanges::coverage}
+#' \item{fwd}{Coverage object for forward strand.}
+#' \item{rev}{Coverage object for reverse strand.}
+#'
+#' @import GenomeInfoDb
+#' @import S4Vectors
+#' @import IRanges
+#' @import GenomicRanges
+#' @importFrom BiocGenerics strand
+#' @importFrom Rsamtools BamFile
+#' @importFrom Rsamtools scanBamFlag
+#' @importFrom Rsamtools ScanBamParam
+#' @importFrom GenomicAlignments readGAlignments
+#'
+#' @export
+#' @examples
+#' bam <- system.file("extdata", "chipseq.bam", package="gcapc")
+#' read5endCoverage(bam)
+
+read5endCoverage <- function(bam,chroms=NULL,mapq=30L,duplicate=FALSE,
+                             flag=scanBamFlag(isUnmappedQuery=FALSE,
+                             isSecondaryAlignment=FALSE,
+                             isNotPassingQualityControls=FALSE)){
+    baminfo <- seqinfo(BamFile(bam))
+    if(!is.null(chroms)) {
+        if(sum(!(chroms %in% seqlevels(baminfo)))>0)
+            stop("chromosome names not compatible with bam files")
+        gal <- readGAlignments(bam,param=ScanBamParam(what = "mapq",
+                   flag = flag, mapqFilter = mapq,
+                   which = GRanges(baminfo)[chroms]))
+        gr <- granges(gal)
+        seqlevels(gr, force=TRUE) <- chroms
+    }else{
+        gal <- readGAlignments(bam,param=ScanBamParam(what = "mapq",
+                   flag = flag, mapqFilter = mapq))
+        gr <- granges(gal)
+    }
+    grf <- resize(gr[strand(gr)=="+"],1)
+    grr <- resize(gr[strand(gr)=="-"],1)
+    covf <- coverage(grf)
+    covr <- coverage(grr)
+    if(!duplicate) {
+        runValue(covf)[runValue(covf)>1] <- 1
+        runValue(covr)[runValue(covr)>1] <- 1
+    }
+    list(fwd=covf,rev=covr)
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# gcapc: GC effects aware peak caller
+
+### Introduction
+ChIP-seq has been widely utilized as the standard technology to detect 
+protein binding regions, where peak calling algorithms were developed 
+particularly to serve the analysis. Existing peak callers lack of power 
+on ranking peaks' significance due to sequencing technology might undergo
+sequence context biases, *e.g.* GC bias. **gcapc** is designed to address 
+this deficiency by modeling GC effects into peak calling.
+
+### Installation
+
+**gcapc** is an R-package using following installation.
+```s
+library(devtools)
+install_github("tengmx/gcapc")
+```
+After installation, the package can be loaded into R.
+
+```s
+library(gcapc)
+```
+
+### Using gcapc
+
+Details of using this package, please see the 
+[vignette](https://github.com/tengmx/gcapc/blob/master/vignettes/gcapc.Rmd).
+
+**gcapc** has been submitted to Bioconductor, and will be available 
+there shortly.
